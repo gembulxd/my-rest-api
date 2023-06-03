c832f5ef9a3772f8f4e38a3a88c0219b2ff0f1e6
gembulxd <132267574+gembulxd@users.noreply.github.com>
2023-06-03 23:30:40 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'AnjasGG'// yourname
 global.MONGO_DB_URI = "mongodb+srv://bowwo924:sX3f7xD7z01GlxhZ@anjass.nutfe8h.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "sX3f7xD7z01GlxhZ" //isi apa aja bebas
-global.your_email = "bowwo924@gmail.com" //email
-global.email_password = "mmugylaqvhbimzgt" //application password email
+global.your_email = "ibukkujsrwatii72@gmail.com" //email
+global.email_password = "cvhrazuegqdunkmb" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
